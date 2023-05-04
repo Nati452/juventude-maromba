58aabb82ca921bcbf698a803ec768994f2d5ceb3
Alex Perez <alexperezscutti@gmail.com>
2023-05-04 11:16:17 -0300

    paginas de treino

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,11 @@ use App\Http\Controllers\TreinoController;
 use App\Http\Controllers\ExercicioController;
 
 
+Route::get('/', [SiteController::class, 'home']);
 
 Route::get('/login', [UsuarioController::class, 'loginView']);
 
-Route::get('/', [SiteController::class, 'home']);
+
 
 Route::get('/exselecionado', [UsuarioController::class, 'exselecionadoView']);
 
@@ -22,6 +23,8 @@ Route::get('/minhaconta', [UsuarioController::class, 'minhacontaView']);
 
 
 Route::get('/pratica', [UsuarioController::class, 'praticaView']);
+
+Route::get('/treinos', [TreinoController::class, 'listagemView']);
 
 
 // As views ficam na pasta resourcer/views
