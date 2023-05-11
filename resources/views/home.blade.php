8a6fc5f64c126a8c89d8ac74b1baf08765449907
Natalia <natisouza452@gmail.com>
2023-05-11 15:03:48 -0300

    barra

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,74 +1,92 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>juventude maromba</title>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="veiewport" content="width=device,width", initial-scale=1.0>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>juventude maromba</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device,width" , initial-scale=1.0>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/nav.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="/css/flex.css">
+  <link rel="stylesheet" href="/css/nav.css">
+  <link rel="stylesheet" href="/css/flex-style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
 </head>
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 <style>
-    body{
-      background-image: url('/img/fundo2.jpg');
-      background-repeat: no-repeat;
-      background-position: center 0%;
-      background-color: blue;
-      background-size: cover;
-      
-    }
-  </style>
+  body {
+    background-image: url('/img/fundo2.jpg');
+    background-repeat: no-repeat;
+    background-position: center 0%;
+    background-color: blue;
+    background-size: cover;
+
+  }
+</style>
+
 <body>
-    <header>
-      <nav>
-        <a class="logo" href="/">tiger</a>
-        <ul class="nav-list">
-          <li><a href="/">pesquisar</a></li>
-      </nav>
-    </header>
+  <header class="flex-row content-vcenter space-40 content-space-between " id="navegador">
+    <span class="material-symbols-outlined">
+      menu
+    </span>
+    <span class="material-symbols-outlined">
+      search
+    </span>
+    <h1><img src="../img/logo.jpg" width="70" height="70" ></h1>
+  </header>
 
 
 
-  
-        <main class="cards">
-        <section class="card contact">
-          <div class="icon">
-          <img src="/img/treinos.png" alt="Contact us.">
-          </div>
-          <h3>Treinos</h3>
-          <span> </span> 
-          <a href="/treinos"><button></button></a>
-        </section>
 
-        <section class="card shop ">
-          <div class="icon">
-          <img src="/img/exercicios.png" alt="Contact us.">
-          </div>
-          
-          <h3>Exercícios</h3>
-          <span></span> 
-         <a href="/exselecionado"><button></button></a>
-        </section>
 
-        <section class="card about">
-          <div class="icon">
-          <img src="/img/conta.png" alt="Contact us.">
-          </div>
-          <h3>Conta</h3>
-          <span></span> 
-          <a href="/minhaconta"><button></button></a>
-        </section>
-        </main>   
-        
+
+
+
+
+
+
+
+
+
+
+
+  <main class="cards">
+    <section class="card contact">
+      <div class="icon">
+        <img src="/img/treinos.png" alt="Contact us.">
+      </div>
+      <h3>Treinos</h3>
+      <span> </span>
+      <a href="/treinos"><button></button></a>
+    </section>
+
+    <section class="card shop ">
+      <div class="icon">
+        <img src="/img/exercicios.png" alt="Contact us.">
+      </div>
+
+      <h3>Exercícios</h3>
+      <span></span>
+      <a href="/exselecionado"><button></button></a>
+    </section>
+
+    <section class="card about">
+      <div class="icon">
+        <img src="/img/conta.png" alt="Contact us.">
+      </div>
+      <h3>Conta</h3>
+      <span></span>
+      <a href="/minhaconta"><button></button></a>
+    </section>
+  </main>
+
 
 </body>
 
