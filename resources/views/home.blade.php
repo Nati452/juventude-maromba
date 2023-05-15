d13370e98e7500fdaf6bcd6db7fbd606e5807771
Natalia <natisouza452@gmail.com>
2023-05-15 16:03:44 -0300

    cabecalho

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,17 +9,16 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device,width" , initial-scale=1.0>
-
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-  <link rel="stylesheet" href="/css/style.css">
   <link rel="stylesheet" href="/css/flex.css">
-  <link rel="stylesheet" href="/css/nav.css">
   <link rel="stylesheet" href="/css/flex-style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <link rel="stylesheet" href="/css/style.css">
 </head>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
 <style>
   body {
     background-image: url('/img/fundo2.jpg');
@@ -32,14 +31,14 @@
 </style>
 
 <body>
-  <header class="flex-row content-vcenter space-40 content-space-between " id="navegador">
-    <span class="material-symbols-outlined">
+  <header class="flex-row content-vcenter content-space-between " id="cabecalho">
+    <span class="material-symbols-outlined" id="menu">
       menu
     </span>
-    <span class="material-symbols-outlined">
-      search
+    <span class="material-symbols-outlined" id="pesquisa">
+        <input type="text" id="caixa">
     </span>
-    <h1><img src="../img/logo.jpg" width="70" height="70" ></h1>
+  <img src="../img/logo.jpg" id="imagem">
   </header>
 
 
@@ -47,24 +46,14 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-  <main class="cards">
+  <main class="cards grow content-center">
     <section class="card contact">
       <div class="icon">
         <img src="/img/treinos.png" alt="Contact us.">
       </div>
-      <h3>Treinos</h3>
+      <h3>  </h3>
       <span> </span>
-      <a href="/treinos"><button></button></a>
+      <a href="/treinos"><button>TREINOS</button></a>
     </section>
 
     <section class="card shop ">
@@ -72,18 +61,18 @@
         <img src="/img/exercicios.png" alt="Contact us.">
       </div>
 
-      <h3>Exercícios</h3>
+      <h3></h3>
       <span></span>
-      <a href="/exselecionado"><button></button></a>
+      <a href="/exselecionado"><button>EXERCÍCIOS</button></a>
     </section>
 
     <section class="card about">
       <div class="icon">
         <img src="/img/conta.png" alt="Contact us.">
       </div>
-      <h3>Conta</h3>
+      <h3></h3>
       <span></span>
-      <a href="/minhaconta"><button></button></a>
+      <a href="/minhaconta"><button>CONTA</button></a>
     </section>
   </main>
 
