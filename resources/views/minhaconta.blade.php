2077568f274c4681027749e259cc1552b8cbfb5d
Natalia <natisouza452@gmail.com>
2023-05-29 13:20:29 -0300

    ajustes

--- resources/views/minhaconta.blade.php
+++ resources/views/minhaconta.blade.php
@@ -9,11 +9,14 @@ body {
   }
   </style>
 
-<div class="flex-row grow  content-center">
+<div class="flex-row grow content-center ">
     <div class="boxconta">
     <img src="../img/iconta.png" id="iconta">
-    <input type="text" class="texto" />
-    <input type="text" class="textoo" />
+    <input type="text"  nome="nome" placeholder="EMAIL"class="texto" />
+  <input type="password" nome="senha" placeholder="SENHA" class="textoo" />
+  <input type="submit" value="ENTRAR"class="entrar"|>
+
+  <P>Ainda não tem conta?<a href="#" >Criar conta</a></P>
     </div>
    
     </div>
