329e89425de3c9d7e8add1c966abf5f5b86c6827
Natalia <natisouza452@gmail.com>
2023-09-04 13:34:55 -0300

    ajustes

--- resources/views/treinos.blade.php
+++ resources/views/treinos.blade.php
@@ -10,7 +10,7 @@
   <div class="flex-row space-40 content-center">
     @for ($i = 1; $i <=36; $i++)
     <a  data-animation="zoom" data-animation-delay="{{ 50 * $i }}"  href="/treino">
-      <img src="../img/bumbum.jpg" id="img3">
+      <img src="../img/biceps.jpg" id="img3">
      
     </a>
   @endfor
